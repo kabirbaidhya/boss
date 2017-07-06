1d3931d88ca63308b0b3d08eed0c197589d05556
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-07-06 23:01:00 +0545

    Show deprecation warning if service config option is found

--- boss/init.py
+++ boss/init.py
@@ -4,11 +4,22 @@ import sys
 from fabric.api import env, task
 from .config import load as load_config, get as get_config, get_stage_config
 from .api.shell import get_stage
+from .util import warn_deprecated
 
 
 def init(module_name):
     ''' Initialize the boss configuration. '''
     config = load_config()
+
+    # This service config option makes it too tightly coupled with
+    # systemd services, so we'll need to make deployment process
+    # independent of systemctl.
+    if config['service'] != None:
+        warn_deprecated(
+            'The `service` configuration option is deprecated' +
+            ' and will be removed in the future releases.'
+        )
+
     stage = get_stage()
     module = sys.modules[module_name]
     define_stage_tasks(module, config)
