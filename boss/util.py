2ac15e84297444d299419f02795c5686817b5023
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-08-15 17:29:15 +0545

    Introduce util.remote_info() function

--- boss/util.py
+++ boss/util.py
@@ -4,6 +4,7 @@ Module for utility functions
 
 import collections
 from copy import deepcopy
+from fabric.api import run as _run, hide
 from fabric.colors import red, green, yellow
 
 
@@ -15,6 +16,12 @@ def halt(msg):
 def info(msg):
     ''' Print a message (Information) '''
     print('\n' + green(msg))
+
+
+def remote_info(msg):
+    ''' Print a message (Information) on the remote logs. '''
+    with hide('running'):
+        _run('echo "{}"'.format(green(msg)))
 
 
 def warn(msg):
@@ -29,12 +36,12 @@ def warn_deprecated(msg):
 
 def merge(dict1, dict2):
     ''' Merge Two dictionaries recursively. '''
-    result = deepcopy(dict1)
+    result=deepcopy(dict1)
 
     for key, value in dict2.iteritems():
         if isinstance(value, collections.Mapping):
-            result[key] = merge(result.get(key, {}), value)
+            result[key]=merge(result.get(key, {}), value)
         else:
-            result[key] = deepcopy(dict2[key])
+            result[key]=deepcopy(dict2[key])
 
     return result
