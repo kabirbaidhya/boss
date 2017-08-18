c0e82a3fb49b0b99795b874869b87f0f063e4f11
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-08-18 16:58:28 +0545

    Introduce fs.mkdir()

--- boss/api/fs.py
+++ boss/api/fs.py
@@ -1,8 +1,18 @@
 ''' File System utilities API. '''
 
 import time
+from . import runner
 
 
 def get_temp_filename(prefix=''):
     ''' Get a unique temporary filename. '''
     return '/tmp/' + (prefix + str(time.time()).replace('.', '-'))
+
+
+def mkdir(path, remote=True, nested=True):
+    ''' Create a new directory. '''
+    options = '-p ' if nested else ''
+    cmd = 'mkdir {0}{1}'.format(options, path)
+
+    # Run the command.
+    runner.run(cmd, remote=remote)
