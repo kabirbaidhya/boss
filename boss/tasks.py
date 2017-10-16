0d1b5d60e3b5f32201dffddc9ec730cfc97d04ba
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-10-16 22:45:39 +0545

    Remove deprecated tailing log from journalctl using service option

--- boss/tasks.py
+++ boss/tasks.py
@@ -4,28 +4,14 @@ Boss default tasks module.
 
 from fabric.api import run as _run, task
 import boss.constants as constants
-from .util import warn_deprecated, halt
+from .util import halt
 from .api import shell, runner
-from .config import get_service, get_stage_config, get as get_config
+from .config import get_stage_config, get as get_config
 
 
 @task
 def logs():
     ''' Tail the logs. '''
-    # Tail the logs from journalctl if
-    # systemctl service is configured
-    # TODO: Remove this (BC Break)
-    if get_service():
-        warn_deprecated(
-            'Using journalctl to tail the logs from ' +
-            'configured service is deprecated. ' +
-            'You\'ll need to provide the config explicitly for logging.'
-        )
-        _run('sudo journalctl -f -u %s' % get_service())
-        return
-
-    # Get the logging config
-
     stage = shell.get_stage()
     stage_specific_logging = get_stage_config(stage).get('logging')
     logging_config = stage_specific_logging or get_config().get('logging')
