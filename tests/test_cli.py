5a583f111f4dbbba8ed126cc72e1ff68435efcc5
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-10-19 11:49:34 +0545

    Use envoy.run instead of popen() and fix all tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,13 @@
-"""Tests for our main boss CLI module."""
+''' Tests for our main boss CLI module. '''
 
-
-from subprocess import PIPE, Popen as popen
+import envoy
 from unittest import TestCase
 
-from boss import NAME, __version__ as VERSION
+from boss import __version__ as VERSION
 
 
 class TestVersion(TestCase):
 
     def test_returns_version_information(self):
-        output = popen(['boss', '--version'], stdout=PIPE).communicate()[0]
-        self.assertEqual(output.strip(), '{} {}'.format(NAME, VERSION))
+        output = envoy.run('boss --version').std_out
+        self.assertEqual(output.strip(), VERSION)
