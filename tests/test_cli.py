8a8e410352ffa5ddc588744b55066eb508f8def3
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-10-19 11:24:16 +0545

    Fix the failing cli tests on python >= 3.x

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,6 +2,7 @@
 
 
 from subprocess import PIPE, Popen as popen
+import envoy
 from unittest import TestCase
 
 from boss import __version__ as VERSION
@@ -10,5 +11,5 @@ from boss import __version__ as VERSION
 class TestVersion(TestCase):
 
     def test_returns_version_information(self):
-        output = popen(['boss', '--version'], stdout=PIPE).communicate()[0]
+        output = envoy.run('boss --version').std_out
         self.assertEqual(output.strip(), VERSION)
