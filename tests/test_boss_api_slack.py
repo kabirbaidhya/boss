bfe254175bd0314c8d711b02e6df8e04f65c5407
Songkran Nethan <songkran.n@kaidee.com>
2017-10-19 16:13:32 +0700

    Add notify_deploying test

--- tests/test_boss_api_slack.py
+++ tests/test_boss_api_slack.py
@@ -4,9 +4,13 @@ from mock import patch
 from boss.api.slack import config
 from boss.api.slack import create_link
 from boss.api.slack import notify
+from boss.api.slack import notify_deploying
 
 
 class TestBoseApiSlack(unittest.TestCase):
+    def setUp(self):
+        self.base_url = config()['base_url'] + config()['endpoint']
+
     def test_create_link(self):
         url = 'http://test-link-url'
         title = 'test link'
@@ -15,7 +19,42 @@ class TestBoseApiSlack(unittest.TestCase):
 
     def test_notify(self):
         with patch('requests.post') as mock_post:
-            base_url = config()['base_url'] + config()['endpoint']
             payload = {"message": "test message"}
             notify(payload)
-            mock_post.assert_called_once_with(base_url, json=payload)
+            mock_post.assert_called_once_with(self.base_url, json=payload)
+
+    def test_notity_deploying(self):
+        notify_params = dict(
+            branch_url='http://branch-url',
+            branch='test_notify_deploying',
+            public_url='http://public-url',
+            host='test-notify-deploying-host',
+            repository_url='http://repository-url',
+            project_name='project-name',
+            server_name='server-name',
+            user='user',
+        )
+        payload = {
+            "text": "user is deploying <http://repository-url|project-name>:<http://branch-url|test_notify_deploying> to <http://public-url|server-name> server.",
+            "attachments": [
+                {
+                    "color": "good",
+                    "fields": [
+                        {
+                            "short": True,
+                            "value": "<http://branch-url|test_notify_deploying>",
+                            "title": "Branch"
+                        },
+                        {
+                            "short": True,
+                            "value": "<http://public-url|test-notify-deploying-host>",
+                            "title": "To"
+                        }
+                    ],
+                    "title": "Deploying"
+                }
+            ]
+        }
+        with patch('requests.post') as mock_post:
+            notify_deploying(**notify_params)
+            mock_post.assert_called_once_with(self.base_url, json=payload)
