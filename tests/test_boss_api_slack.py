07ef5b452c5e9e59826c7a1d12d79b954d3f0355
Songkran Nethan <songkran.n@kaidee.com>
2017-10-19 15:59:19 +0700

    Add test notify

--- tests/test_boss_api_slack.py
+++ tests/test_boss_api_slack.py
@@ -1,6 +1,9 @@
 import unittest
+from mock import patch
 
+from boss.api.slack import config
 from boss.api.slack import create_link
+from boss.api.slack import notify
 
 
 class TestBoseApiSlack(unittest.TestCase):
@@ -9,3 +12,10 @@ class TestBoseApiSlack(unittest.TestCase):
         title = 'test link'
         expected_link = '<{url}|{title}>'.format(url=url, title=title)
         self.assertEqual(create_link(url, title), expected_link)
+
+    def test_notify(self):
+        with patch('requests.post') as mock_post:
+            base_url = config()['base_url'] + config()['endpoint']
+            payload = {"message": "test message"}
+            notify(payload)
+            mock_post.assert_called_once_with(base_url, json=payload)
