f93c2199aa0a0a8c52967a43ce88bb45152fe726
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-10-24 00:31:15 +0545

    Add test for boss cli init command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,27 @@
-''' Tests for our main boss CLI module. '''
+''' Tests for CLI. '''
 
+import os
 import envoy
-from unittest import TestCase
 
+from click.testing import CliRunner
 from boss import __version__ as VERSION
+from boss.constants import DEFAULT_CONFIG_FILE, FABFILE_PATH
+from boss.cli import main as cli
 
 
-class TestVersion(TestCase):
+def test_version_option():
+    ''' Test version option. '''
+    output = envoy.run('boss --version').std_out
+    assert output.strip() == VERSION
 
-    def test_version_option_returns_package_version(self):
-        output = envoy.run('boss --version').std_out
-        self.assertEqual(output.strip(), VERSION)
+
+def test_init_command():
+    ''' Test init command generates files. '''
+    runner = CliRunner()
+
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ['init'])
+
+        assert result.exit_code == 0
+        assert os.path.exists(FABFILE_PATH)
+        assert os.path.exists(DEFAULT_CONFIG_FILE)
