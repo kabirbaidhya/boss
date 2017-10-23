5cef8b4e2be11a47bc7d2ea0019e74710a690654
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-10-23 22:55:14 +0545

    Add boss cli init command

--- boss/cli.py
+++ boss/cli.py
@@ -1,8 +1,25 @@
 from . import __version__
 import click
 
+from .core import initializer
+
 
 @click.version_option(__version__, message='%(version)s')
-@click.command()
+@click.group()
 def main():
-    click.echo('Boss says!')
+    ''' Boss CLI. '''
+    pass
+
+
+@main.command('init')
+def init():
+    ''' Initialize a project directory for boss. '''
+    files_written = initializer.initialize()
+
+    if not files_written:
+        click.echo('Already initialized.')
+        return
+
+    # Print the files generated while initializing.
+    for f in files_written:
+        click.echo('Generated file: {}'.format(f))
