fe8a00462836af5b47e3524a66be11bf74aed605
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-10-24 01:02:31 +0545

    Rewrite tests to avoid class based tests

--- tests/core/test_fs.py
+++ tests/core/test_fs.py
@@ -1,36 +1,34 @@
+''' Tests for boss.core.fs module. '''
 
-from unittest import TestCase
 from mock import patch, mock_open
-
 from boss.core import fs
 
 
-class TestFs(TestCase):
-    ''' Tests for boss.core.fs module. '''
+@patch('os.path.exists')
+def test_exists(result):
+    ''' Test fs.exists() works. '''
+    filename = 'somefile'
+    fs.exists(filename)
+    result.assert_called_with(filename)
 
-    @patch('os.path.exists')
-    def test_exists(self, result):
-        ''' Test fs.exists() works. '''
-        filename = 'somefile'
-        fs.exists(filename)
-        result.assert_called_with(filename)
 
-    def test_read(self):
-        ''' Test fs.read() works. '''
-        filename = 'somefile'
-        data = 'somedata'
+def test_read():
+    ''' Test fs.read() works. '''
+    filename = 'somefile'
+    data = 'somedata'
 
-        with patch('__builtin__.open', mock_open(read_data=data)) as mock_file:
-            assert fs.read(filename) == data
-            mock_file.assert_called_with(filename, 'r')
+    with patch('__builtin__.open', mock_open(read_data=data)) as mock_file:
+        assert fs.read(filename) == data
+        mock_file.assert_called_with(filename, 'r')
 
-    def test_write(self):
-        ''' Test fs.write() works. '''
-        filename = 'somefile'
-        data = 'somedata'
-        m = mock_open()
 
-        with patch('__builtin__.open', m) as mock_file:
-            fs.write(filename, data)
-            mock_file.assert_called_with(filename, 'w')
-            m().write.assert_called_with(data)
+def test_write():
+    ''' Test fs.write() works. '''
+    filename = 'somefile'
+    data = 'somedata'
+    m = mock_open()
+
+    with patch('__builtin__.open', m) as mock_file:
+        fs.write(filename, data)
+        mock_file.assert_called_with(filename, 'w')
+        m().write.assert_called_with(data)
--- tests/test_boss_api_slack.py
+++ tests/test_boss_api_slack.py
@@ -1,96 +1,101 @@
-import unittest
+''' Tests for boss.api.slack module. '''
+
 from mock import patch
-
-from boss.api.slack import config
-from boss.api.slack import create_link
-from boss.api.slack import notify
-from boss.api.slack import notify_deploying
+from boss.api import slack
 
 
-class TestBoseApiSlack(unittest.TestCase):
-    def setUp(self):
-        self.base_url = config()['base_url'] + config()['endpoint']
+def test_create_link():
+    ''' Test slack.create_link(). '''
+    url = 'http://test-link-url'
+    title = 'Test link'
+    expected_link = '<{url}|{title}>'.format(url=url, title=title)
+    assert slack.create_link(url, title) == expected_link
 
-    def test_create_link(self):
-        url = 'http://test-link-url'
-        title = 'Test link'
-        expected_link = '<{url}|{title}>'.format(url=url, title=title)
-        self.assertEqual(create_link(url, title), expected_link)
 
-    def test_notify(self):
-        with patch('requests.post') as mock_post:
-            payload = {"message": "Test message"}
-            notify(payload)
-            mock_post.assert_called_once_with(self.base_url, json=payload)
+def test_notify():
+    ''' Test slack.notify(). '''
+    base_url = slack.config()['base_url'] + slack.config()['endpoint']
 
-    def test_notity_deploying(self):
-        notify_params = dict(
-            branch_url='http://branch-url',
-            branch='test_notify_deploying',
-            public_url='http://public-url',
-            host='test-notify-deploying-host',
-            repository_url='http://repository-url',
-            project_name='project-name',
-            server_name='server-name',
-            user='user',
-        )
-        payload = {
-            "text": "user is deploying <http://repository-url|project-name>:<http://branch-url|test_notify_deploying> to <http://public-url|server-name> server.",
-            "attachments": [
-                {
-                    "color": "good",
-                    "fields": [
-                        {
-                            "short": True,
-                            "value": "<http://branch-url|test_notify_deploying>",
-                            "title": "Branch"
-                        },
-                        {
-                            "short": True,
-                            "value": "<http://public-url|test-notify-deploying-host>",
-                            "title": "To"
-                        }
-                    ],
-                    "title": "Deploying"
-                }
-            ]
-        }
-        with patch('requests.post') as mock_post:
-            notify_deploying(**notify_params)
-            mock_post.assert_called_once_with(self.base_url, json=payload)
+    with patch('requests.post') as mock_post:
+        payload = {"message": "Test message"}
+        slack.notify(payload)
+        mock_post.assert_called_once_with(base_url, json=payload)
 
-    def test_notity_deployed(self):
-        notify_params = dict(
-            branch_url='http://branch-url',
-            branch='test_notify_deployed',
-            public_url='http://public-url',
-            host='test-notify-deployed-host',
-            repository_url='http://repository-url',
-            project_name='project-name',
-            server_name='server-name',
-            user='user',
-        )
-        payload = {
-            "text": "user is deploying <http://repository-url|project-name>:<http://branch-url|test_notify_deployed> to <http://public-url|server-name> server.",
-            "attachments": [
-                {
-                    "color": "good",
-                    "fields": [
-                        {
-                            "short": True,
-                            "value": "<http://branch-url|test_notify_deployed>",
-                            "title": "Branch"
-                        },
-                        {
-                            "short": True,
-                            "value": "<http://public-url|test-notify-deployed-host>",
-                            "title": "To"
-                        }
-                    ],
-                    "title": "Deploying"
-                }
-            ]
-        }
-        with patch('requests.post') as mock_post:
-            notify_deploying(**notify_params)
-            mock_post.assert_called_once_with(self.base_url, json=payload)
+
+def test_notity_deploying():
+    ''' Test slack.notify_deploying(). '''
+    notify_params = dict(
+        branch_url='http://branch-url',
+        branch='test_notify_deploying',
+        public_url='http://public-url',
+        host='test-notify-deploying-host',
+        repository_url='http://repository-url',
+        project_name='project-name',
+        server_name='server-name',
+        user='user',
+    )
+    payload = {
+        "text": "user is deploying <http://repository-url|project-name>:<http://branch-url|test_notify_deploying> to <http://public-url|server-name> server.",
+        "attachments": [
+            {
+                "color": "good",
+                "fields": [
+                    {
+                        "short": True,
+                        "value": "<http://branch-url|test_notify_deploying>",
+                        "title": "Branch"
+                    },
+                    {
+                        "short": True,
+                        "value": "<http://public-url|test-notify-deploying-host>",
+                        "title": "To"
+                    }
+                ],
+                "title": "Deploying"
+            }
+        ]
+    }
+    base_url = slack.config()['base_url'] + slack.config()['endpoint']
+    with patch('requests.post') as mock_post:
+        slack.notify_deploying(**notify_params)
+        mock_post.assert_called_once_with(base_url, json=payload)
+
+
+def test_notity_deployed():
+    ''' Test slack.notify_deployed(). '''
+    notify_params = dict(
+        branch_url='http://branch-url',
+        branch='test_notify_deployed',
+        public_url='http://public-url',
+        host='test-notify-deployed-host',
+        repository_url='http://repository-url',
+        project_name='project-name',
+        server_name='server-name',
+        user='user',
+    )
+    payload = {
+        "text": "user is deploying <http://repository-url|project-name>:<http://branch-url|test_notify_deployed> to <http://public-url|server-name> server.",
+        "attachments": [
+            {
+                "color": "good",
+                "fields": [
+                    {
+                        "short": True,
+                        "value": "<http://branch-url|test_notify_deployed>",
+                        "title": "Branch"
+                    },
+                    {
+                        "short": True,
+                        "value": "<http://public-url|test-notify-deployed-host>",
+                        "title": "To"
+                    }
+                ],
+                "title": "Deploying"
+            }
+        ]
+    }
+    base_url = slack.config()['base_url'] + slack.config()['endpoint']
+
+    with patch('requests.post') as mock_post:
+        slack.notify_deploying(**notify_params)
+        mock_post.assert_called_once_with(base_url, json=payload)
