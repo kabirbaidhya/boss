d95e1c2a24e35bf8c9495dbf2ebbd018b1de0180
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-11-15 23:17:02 +0545

    Add another test for buildman.resolve_local_build_dir()
    Test resolve_local_build_dir() returns one of the default build directories, if build_dir is not provided and fallback directories don't exist either.

--- tests/api/deployment/test_buildman.py
+++ tests/api/deployment/test_buildman.py
@@ -1,21 +1,42 @@
 ''' Tests for boss.api.deployment.buildman module. '''
 
 from mock import patch
+from boss.api.deployment import buildman
 
 
-@patch('boss.config.get_stage_config')
-def test_resolve_local_build_dir_when_build_dir_is_configured(stage_config_mock):
+@patch('boss.api.deployment.buildman.get_stage_config')
+def test_resolve_local_build_dir_when_build_dir_is_configured(gsc_mock):
     '''
     Test resolve_local_build_dir() returns configured value if build_dir is configured.
     '''
 
-    from boss.api.deployment import buildman
+    # with patch('boss.api.deployment.buildman.get_stage_config') as gsc_mock:
 
     test_build_dir = 'my-build-directory'
-    stage_config_mock.return_value = {
+    gsc_mock.return_value = {
         'deployment': {
             'build_dir': test_build_dir
         }
     }
 
     assert buildman.resolve_local_build_dir() == test_build_dir
+
+
+@patch('os.path.exists')
+@patch('boss.api.deployment.buildman.get_stage_config')
+def test_resolve_local_build_dir_when_build_dir_is_none(gsc_mock, exists_mock):
+    '''
+    Test resolve_local_build_dir() returns one of the default
+    build directories, if build_dir is not provided and
+    fallback directories don't exist either.
+    '''
+    exists_mock.return_value = False
+    gsc_mock.return_value = {
+        'deployment': {
+            'build_dir': None
+        }
+    }
+
+    result = buildman.resolve_local_build_dir()
+
+    assert result in buildman.LOCAL_BUILD_DIRECTORIES
