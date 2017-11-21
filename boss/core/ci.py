c67fd3af1c3ef595573c89343587ffc92c8aa547
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-11-21 22:02:12 +0545

    Ensure all tests passes even on CI environment

--- boss/core/ci.py
+++ boss/core/ci.py
@@ -7,7 +7,8 @@ def is_ci():
     ''' Check if boss is running in a Continuous Integration (CI) environment. '''
 
     return bool(
-        (env.get('CI') and env['CI'] == 'true') or
-        (env.get('CONTINUOUS_INTEGRATION')
-         and env['CONTINUOUS_INTEGRATION'] == 'true')
+        env.get('BOSS_RUNNING') == 'true' and (
+            (env.get('CI') == 'true') or
+            (env.get('CONTINUOUS_INTEGRATION') == 'true')
+        )
     )
--- tests/core/test_ci.py
+++ tests/core/test_ci.py
@@ -9,6 +9,7 @@ def test_is_ci_returns_true_case_1():
     Test is_ci() returns True if CI=true,
     is passed through environment variables.
     '''
+    os.environ['BOSS_RUNNING'] = 'true'
     os.environ['CI'] = 'true'
 
     assert is_ci() is True
@@ -19,6 +20,7 @@ def test_is_ci_returns_true_case_2():
     Test is_ci() returns True if CONTINUOUS_INTEGRATION=true,
     is passed through environment variables.
     '''
+    os.environ['BOSS_RUNNING'] = 'true'
     os.environ['CONTINUOUS_INTEGRATION'] = 'true'
 
     assert is_ci() is True
@@ -26,10 +28,22 @@ def test_is_ci_returns_true_case_2():
 
 def test_is_ci_returns_false():
     '''
-    Test is_ci() returns True if CONTINUOUS_INTEGRATION=true,
-    is passed through environment variables.
+    Test is_ci() returns False if CI related environment variables,
+    are not found.
     '''
     os.environ['CI'] = ''
     os.environ['CONTINUOUS_INTEGRATION'] = ''
+
+    assert is_ci() is False
+
+
+def test_is_ci_returns_false_if_boss_is_not_running_in_run_mode():
+    '''
+    Test is_ci() returns False if boss is not running (test environment).
+    (This is to check is_ci() function works well when these tests are running in CI).
+    '''
+    os.environ['BOSS_RUNNING'] = ''
+    os.environ['CI'] = 'true'
+    os.environ['CONTINUOUS_INTEGRATION'] = 'true'
 
     assert is_ci() is False
