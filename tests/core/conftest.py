47ed7448d4d83b774f427feebc7fd67d73954698
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-12-17 22:43:36 +0545

    Set scope=function for server fixture

--- tests/core/conftest.py
+++ tests/core/conftest.py
@@ -7,7 +7,7 @@ from boss import BASE_PATH
 SAMPLE_USER_KEY = os.path.join(BASE_PATH, 'misc/sample-keys/user-key')
 
 
-@yield_fixture()
+@yield_fixture(scope='function')
 def server():
     ''' Mock SSH Server. '''
     users = {'sample-user': SAMPLE_USER_KEY}
