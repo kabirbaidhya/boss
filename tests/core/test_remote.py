d3a03a66279876b8f6da40c169aad9fecf04704a
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-12-17 22:36:14 +0545

    Add unit test for remote get operation

--- tests/core/test_remote.py
+++ tests/core/test_remote.py
@@ -4,30 +4,45 @@ Tests for the boss.core.remote module.
 
 import pytest
 from mock import Mock
-from boss.core.remote import put
+from boss.core import remote
 
 
 @pytest.fixture()
 def callback():
+    ''' Callback to be used for sftp get() and put(). '''
     return lambda x, y: None
 
 
-def test_put(callback):
+@pytest.fixture()
+def sftp():
+    ''' Get the mocked sftp client. '''
+    return Mock()
+
+
+def test_put(sftp, callback):
     ''' Test put() works. '''
-    sftp_client = Mock()
     local_path = 'test.yml'
     remote_path = '~/test.yml'
 
-    put(
-        sftp_client,
+    remote.put(
+        sftp,
         local_path=local_path,
         remote_path=remote_path,
         callback=callback,
         confirm=False
     )
-    sftp_client.put.assert_called_with(
-        local_path,
-        remote_path,
-        callback,
-        False
+    sftp.put.assert_called_with(local_path, remote_path, callback, False)
+
+
+def test_get(sftp, callback):
+    ''' Test get() works. '''
+    local_path = 'test.yml'
+    remote_path = '~/test.yml'
+
+    remote.get(
+        sftp,
+        remote_path=remote_path,
+        local_path=local_path,
+        callback=callback
     )
+    sftp.get.assert_called_with(remote_path, local_path, callback)
