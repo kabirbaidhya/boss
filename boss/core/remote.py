ba31b34b34cb933269da8133c0fceae98efb443e
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-12-17 20:22:35 +0545

    Add boss.core.remote.get() function

--- boss/core/remote.py
+++ boss/core/remote.py
@@ -1,13 +1,15 @@
 '''
 A core utility module to deal with remote host and operations
 over SSH connection.
-Uses paramiko directly for remote execution and transfers.
+
+This is an abstraction over the underlying SSH/SFTP transport
+which uses paramiko directly for remote execution and transfers.
 '''
 
 
 def put(sftp_client, **params):
     '''
-    Transfers a local file to the remote path via SFTP.
+    Transfers (upload) a local file to the remote path via SFTP.
     '''
 
     local_path = params['local_path']
@@ -15,5 +17,16 @@ def put(sftp_client, **params):
     callback = params.get('callback')
     confirm = params.get('confirm')
 
-    # Do the put operation.
     return sftp_client.put(local_path, remote_path, callback, confirm)
+
+
+def get(sftp_client, **params):
+    '''
+    Transfers (download) a remote file to the local path via SFTP.
+    '''
+
+    remote_path = params['remote_path']
+    local_path = params['local_path']
+    callback = params.get('callback')
+
+    return sftp_client.get(remote_path, local_path, callback)
