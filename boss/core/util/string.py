6187e375491dcf853a33000eb7451dc7ef24f1e0
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-12-23 01:43:26 +0545

    Introduce core string utility function is_quoted()

--- boss/core/util/string.py
+++ boss/core/util/string.py
@@ -8,3 +8,12 @@ ANSI_CODES_REGEX = '\x1b[^m]*m'
 def strip_ansi(text):
     ''' Strip ANSI escape character codes from a string. '''
     return re.sub(ANSI_CODES_REGEX, '', text)
+
+
+def is_quoted(string):
+    ''' Check if the provided string is quoted. '''
+    return (
+        len(string) > 0 and
+        string[0] == string[-1] and
+        string[0] in ['"', "'"]
+    )
