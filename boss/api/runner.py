809344e634af83fc0d15e0d3c79e632f021562b5
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-12-26 00:48:12 +0545

    Send script running notification for scripts to be notified

--- boss/api/runner.py
+++ boss/api/runner.py
@@ -1,8 +1,14 @@
 
-from boss.core.util.colors import cyan
 from fabric.api import run as _run, local as _local, hide
-from ..config import get as _get_config
-from ..util import host_info
+
+from boss.api import notif, shell
+from boss.util import host_info
+from boss.config import get as _get_config
+from boss.core.util.colors import cyan
+from boss.core.constants.notification_types import (
+    RUNNING_SCRIPT_STARTED,
+    RUNNING_SCRIPT_FINISHED
+)
 
 
 def run(command, remote=True):
@@ -50,7 +56,16 @@ def run_script(script, remote=True):
     if not is_script_defined(script):
         raise RuntimeError('Missing script "{}"'.format(script))
 
-    # Get the command defined in the script.
+    needs_notification = should_notify(script)
+
+    if needs_notification:
+        notif.send(RUNNING_SCRIPT_STARTED, {
+            'script': script,
+            'user': shell.get_user(),
+            'stage': shell.get_stage()
+        })
+
+        # Get the command defined in the script.
     script_cmd = custom_scripts[script]
 
     info_text = 'Running {}\n{}'.format(
@@ -61,3 +76,10 @@ def run_script(script, remote=True):
     # Run a custom script defined in the config.
     with hide('running'):
         run(script_cmd, remote)
+
+    if needs_notification:
+        notif.send(RUNNING_SCRIPT_FINISHED, {
+            'script': script,
+            'user': shell.get_user(),
+            'stage': shell.get_stage()
+        })
