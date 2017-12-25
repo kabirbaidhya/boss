3bf5ed64a26d83dfbd0c3e6608cde5e5b5e1363b
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-12-26 01:03:47 +0545

    Test to ensure scripts are being notified

--- tests/api/test_runner.py
+++ tests/api/test_runner.py
@@ -2,7 +2,11 @@
 
 from mock import patch
 
-from boss.api.runner import should_notify
+from boss.api.runner import should_notify, run_script
+from boss.core.constants.notification_types import (
+    RUNNING_SCRIPT_STARTED,
+    RUNNING_SCRIPT_FINISHED
+)
 
 
 @patch('boss.api.runner._get_config')
@@ -44,3 +48,30 @@ def test_should_notify_returns_false(gc_m):
 
     assert not should_notify('foo')
     assert not should_notify('bar')
+
+
+@patch('boss.api.runner._run')
+@patch('boss.api.runner.hide')
+@patch('boss.api.runner.host_info')
+@patch('boss.api.runner._get_config')
+@patch('boss.api.runner.notif.send')
+def test_run_script_send_script_running_notifications(send_m, gc_m, hi_m, h_m, r_m):
+    gc_m.return_value = {
+        'scripts': {
+            'foo': 'just foo'
+        },
+        'notified_hooks': {
+            'scripts': ['foo']
+        }
+    }
+
+    run_script('foo')
+
+    send_m.assert_called()
+    (call1, call2) = send_m.call_args_list
+
+    assert call1[0][0] == RUNNING_SCRIPT_STARTED
+    assert call1[0][1]['script'] == 'foo'
+
+    assert call2[0][0] == RUNNING_SCRIPT_FINISHED
+    assert call2[0][1]['script'] == 'foo'
