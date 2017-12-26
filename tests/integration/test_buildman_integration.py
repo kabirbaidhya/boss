5924a9a3851ed2949f486f44c96116f52c710db0
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-12-27 00:27:29 +0545

    Integration test to ensure buildman.save_history() works

--- tests/integration/test_buildman_integration.py
+++ tests/integration/test_buildman_integration.py
@@ -10,6 +10,7 @@ from boss.api.deployment import buildman
 
 
 def test_load_history(server):
+    ''' Test load_history() works. '''
     for uid in server.users:
         path = os.path.join(server.ROOT_DIR, 'testfile.json')
         fs.write(path, '{"foo": "bar", "hello": "world"}')
@@ -24,3 +25,27 @@ def test_load_history(server):
 
                     assert result['foo'] == 'bar'
                     assert result['hello'] == 'world'
+
+
+def test_save_history(server):
+    ''' Test save_history() works. '''
+
+    for uid in server.users:
+        path = os.path.join(server.ROOT_DIR, 'history.json')
+
+        assert not fs.exists(path)
+
+        with server.client(uid) as client:
+            with patch('boss.api.deployment.buildman.get_builds_file') as gbf_m:
+                gbf_m.return_value = path
+
+                with patch('boss.api.ssh.resolve_sftp_client') as rsc_m:
+                    rsc_m.return_value = client.open_sftp()
+                    buildman.save_history({
+                        'foo': 'bar',
+                        'hello': 'world'
+                    })
+
+                    result = fs.read(path)
+
+                    assert result == '{"foo": "bar", "hello": "world"}'
