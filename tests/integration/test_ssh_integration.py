ba58682650876b5a9650f4e2f9c02c81fba6269c
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-12-31 14:49:31 +0545

    Integration tests for ssh.upload_dir()

---
+++ tests/integration/test_ssh_integration.py
@@ -0,0 +1,42 @@
+''' Integration tests for ssh module. '''
+
+
+import os
+from tempfile import mkdtemp
+from mock import patch
+
+from boss.core import fs
+from boss.api.ssh import upload_dir
+
+
+def test_upload_dir(server):
+    ''' Test upload_dir() transfers a local directory to the remote end. '''
+
+    # Setup local directory.
+    local_dir = os.path.join(mkdtemp(), 'test')
+
+    os.mkdir(local_dir)
+    os.mkdir(os.path.join(local_dir, 'a'))
+    os.mkdir(os.path.join(local_dir, 'b'))
+
+    fs.write(os.path.join(local_dir, 'a/foo.txt'), 'Foo')
+    fs.write(os.path.join(local_dir, 'b/bar.txt'), 'Bar')
+
+    for uid in server.users:
+        remote_path = os.path.join(server.ROOT_DIR, 'remote')
+
+        with server.client(uid) as client:
+            with patch('boss.api.ssh.resolve_client') as rc_m:
+                rc_m.return_value = client
+
+                assert not fs.exists(remote_path)
+
+                # Upload the directory
+                upload_dir(local_dir, remote_path)
+
+                # Test the whole directory got uploaded with all files
+                assert fs.exists(remote_path)
+                assert fs.exists(os.path.join(remote_path, 'a'))
+                assert fs.exists(os.path.join(remote_path, 'b'))
+                assert fs.read(os.path.join(remote_path, 'a/foo.txt')) == 'Foo'
+                assert fs.read(os.path.join(remote_path, 'b/bar.txt')) == 'Bar'
