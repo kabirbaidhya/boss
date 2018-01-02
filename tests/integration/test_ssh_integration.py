130bd4bb6f1d76a6b2cce53891c9494804043a1d
Kabir Baidhya <kabirbaidhya@gmail.com>
2018-01-02 22:04:54 +0545

    Test to ensure ssh.run() runs single and multipe commands correctly

--- tests/integration/test_ssh_integration.py
+++ tests/integration/test_ssh_integration.py
@@ -111,3 +111,30 @@ def test_put_when_remote_directory_provided(server):
 
                 assert fs.exists(expected_target_file)
                 assert fs.read(expected_target_file) == 'Test put operation'
+
+
+def test_run(server):
+    ''' Test run() executes a command over ssh. '''
+    for uid in server.users:
+        with server.client(uid) as client:
+            with patch('boss.api.ssh.resolve_client') as rc_m:
+                rc_m.return_value = client
+                result = ssh.run('echo "hello"')
+
+                assert 'hello' in result
+
+
+def test_run_list_of_commands(server):
+    ''' Test run() executes a series of commands over ssh. '''
+    for uid in server.users:
+        with server.client(uid) as client:
+            with patch('boss.api.ssh.resolve_client') as rc_m:
+                rc_m.return_value = client
+                result = ssh.run([
+                    'echo "hello"',
+                    'echo "world"',
+                    'echo "foo"',
+                    'echo "bar"'
+                ])
+
+                assert result == ['hello', 'world', 'foo', 'bar']
