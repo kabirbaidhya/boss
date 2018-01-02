398606ca6cf04033813c18f7e8c7b99f4f066feb
Kabir Baidhya <kabirbaidhya@gmail.com>
2018-01-03 01:17:29 +0545

    Rename Uploader to DirectoryUploader

--- boss/api/transfers.py
+++ boss/api/transfers.py
@@ -10,9 +10,9 @@ from boss.core.util.colors import green, cyan
 from boss.api.ssh import run, put, normalize_path
 
 
-class Uploader(object):
+class DirectoryUploader(object):
     '''
-    Uploader
+    DirectoryUploader
     --------
     A utility class for uploading files and directories easily.
     '''
@@ -28,7 +28,7 @@ class Uploader(object):
     STATUS_DONE = 8
 
     def __init__(self, local_path, callback=None):
-        ''' Uploader constructor. '''
+        ''' DirectoryUploader constructor. '''
         tmp_folder = mkdtemp()
         self.local_path = local_path
         self.name = 'upload-{}.tar.gz'.format(str(time()).replace('.', '-'))
@@ -45,27 +45,29 @@ class Uploader(object):
 
     def upload(self, remote_path):
         ''' Start the upload operation. '''
-        self.update(Uploader.STATUS_PREPARING)
+        self.update(DirectoryUploader.STATUS_PREPARING)
         remote_path = normalize_path(remote_path)
 
         # Compress the directory.
-        self.update(Uploader.STATUS_COMPRESSING)
+        self.update(DirectoryUploader.STATUS_COMPRESSING)
         compress(self.local_path, self.tar_path)
 
         total_size = os.path.getsize(self.tar_path)
-        self.update(Uploader.STATUS_COMPRESSED, total=total_size)
+        self.update(DirectoryUploader.STATUS_COMPRESSED, total=total_size)
 
         def put_callback(sent, total):
-            self.update(Uploader.STATUS_UPLOADING, sent=sent, total=total)
+            self.update(DirectoryUploader.STATUS_UPLOADING,
+                        sent=sent, total=total)
 
         # Upload the tar zipped file to the remote.
         # The compressed folder gets uploaded to a temp path first.
         # Then later is extracted to the provided path on the remote.
-        self.update(Uploader.STATUS_PREPARING_TO_UPLOAD, total=total_size)
+        self.update(DirectoryUploader.STATUS_PREPARING_TO_UPLOAD,
+                    total=total_size)
         put(self.tar_path, self.remote_tmp_path, put_callback)
 
         # Extract the files to the remote directory
-        self.update(Uploader.STATUS_FINALIZING)
+        self.update(DirectoryUploader.STATUS_FINALIZING)
         run([
             'mkdir -p {}'.format(remote_path),
             'tar zxvf {src} --strip-components=1 -C {dest}'.format(
@@ -75,35 +77,35 @@ class Uploader(object):
         ])
 
         os.remove(self.tar_path)
-        self.update(Uploader.STATUS_DONE)
+        self.update(DirectoryUploader.STATUS_DONE)
 
 
 # Default status message map
 DEFAULT_MESSAGES = {}
-DEFAULT_MESSAGES[Uploader.STATUS_PREPARING] = 'Preparing'
-DEFAULT_MESSAGES[Uploader.STATUS_COMPRESSING] = 'Compressing'
-DEFAULT_MESSAGES[Uploader.STATUS_COMPRESSED] = 'Compressed'
-DEFAULT_MESSAGES[Uploader.STATUS_PREPARING_TO_UPLOAD] = 'Uploading'
-DEFAULT_MESSAGES[Uploader.STATUS_UPLOADING] = 'Uploading'
-DEFAULT_MESSAGES[Uploader.STATUS_UPLOADED] = 'Uploaded'
-DEFAULT_MESSAGES[Uploader.STATUS_FINALIZING] = 'Finalizing'
-DEFAULT_MESSAGES[Uploader.STATUS_DONE] = 'Upload Completed'
+DEFAULT_MESSAGES[DirectoryUploader.STATUS_PREPARING] = 'Preparing'
+DEFAULT_MESSAGES[DirectoryUploader.STATUS_COMPRESSING] = 'Compressing'
+DEFAULT_MESSAGES[DirectoryUploader.STATUS_COMPRESSED] = 'Compressed'
+DEFAULT_MESSAGES[DirectoryUploader.STATUS_PREPARING_TO_UPLOAD] = 'Uploading'
+DEFAULT_MESSAGES[DirectoryUploader.STATUS_UPLOADING] = 'Uploading'
+DEFAULT_MESSAGES[DirectoryUploader.STATUS_UPLOADED] = 'Uploaded'
+DEFAULT_MESSAGES[DirectoryUploader.STATUS_FINALIZING] = 'Finalizing'
+DEFAULT_MESSAGES[DirectoryUploader.STATUS_DONE] = 'Upload Completed'
 
 
 def default_status_message(status, **params):
-    ''' Default status callback function for Uploader. '''
+    ''' Default status callback function for DirectoryUploader. '''
     message = green(DEFAULT_MESSAGES[status])
     blank = '\r' + (' ' * 50) + '\r'  # Blank padding to clear the output line
     result = blank + message
 
-    if status == Uploader.STATUS_PREPARING:
+    if status == DirectoryUploader.STATUS_PREPARING:
         result = '\n' + result
-    elif status == Uploader.STATUS_PREPARING_TO_UPLOAD:
+    elif status == DirectoryUploader.STATUS_PREPARING_TO_UPLOAD:
         result = blank + '{} [{}]'.format(
             message,
             cyan(size_unit(params['total']))
         )
-    elif status == Uploader.STATUS_UPLOADING:
+    elif status == DirectoryUploader.STATUS_UPLOADING:
         sent = params['sent']
         total = params['total']
         progress = (sent * 100.0 / total)
@@ -111,7 +113,7 @@ def default_status_message(status, **params):
             message,
             cyan(size_unit(total)), progress
         )
-    elif status == Uploader.STATUS_DONE:
+    elif status == DirectoryUploader.STATUS_DONE:
         result += '\n\n'
 
     return result
@@ -119,6 +121,6 @@ def default_status_message(status, **params):
 
 def upload_dir(local_path, remote_path, callback=None):
     ''' Upload local directory to the remote. '''
-    uploader = Uploader(local_path, callback)
+    uploader = DirectoryUploader(local_path, callback)
 
     return uploader.upload(remote_path)
