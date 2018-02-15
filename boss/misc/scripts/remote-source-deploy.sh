82db2a17f465d8406a8a7bf5dd732e6eb43f4835
Kabir Baidhya <kabirbaidhya@gmail.com>
2018-02-16 00:39:35 +0545

    Add a deploy shell script instead of sync.sh

--- boss/misc/scripts/remote-source-deploy.sh
+++ boss/misc/scripts/remote-source-deploy.sh
@@ -11,7 +11,27 @@ if [ -d ".git" ]; then
   git checkout -f $BRANCH
 else
   git clone -b $BRANCH  $REPOSITORY_URL $REPOSITORY_PATH
-fi;
+fi
 
 printf "\nSynchronizing with the latest changes on branch ${BRANCH}.\n"
 git reset --hard origin/$BRANCH
+
+if [ ! -z "$SCRIPT_INSTALL" ]; then
+  printf "\n> $SCRIPT_INSTALL\n";
+  $SCRIPT_INSTALL;
+fi
+
+if [ ! -z "$SCRIPT_BUILD" ]; then
+  printf "\n> $SCRIPT_BUILD\n";
+  $SCRIPT_BUILD;
+fi
+
+if [ ! -z "$SCRIPT_RELOAD" ]; then
+  printf "\n> $SCRIPT_RELOAD\n";
+  $SCRIPT_RELOAD;
+fi
+
+if [ ! -z "$SCRIPT_STATUS_CHECK\n" ]; then
+  printf "\n> $SCRIPT_STATUS_CHECK";
+  $SCRIPT_STATUS_CHECK;
+fi
