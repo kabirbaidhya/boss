d45bd91ea536afafb2e26e3c887dce645b139ca2
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-06-22 23:24:56 +0545

    Bump version 1.0.1

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Deploy like a boss.
 ## Installation
 
 ```bash
-$ pip install boss-cli==1.0.0
+$ pip install boss-cli==1.0.1
 ```
 
 ## Configuration
--- boss/__init__.py
+++ boss/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '1.0.1-alpha.1'
+__version__ = '1.0.1'
 
 NAME = 'boss'
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
