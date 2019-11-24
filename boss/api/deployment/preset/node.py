0404f9a9c8c675e866792e6d7f9d0fc0a093f969
Saugat Acharya <mesaugat@gmail.com>
2019-11-24 20:07:17 +0545

    Add new task to build code locally

--- boss/api/deployment/preset/node.py
+++ boss/api/deployment/preset/node.py
@@ -58,6 +58,15 @@ def upload_included_files(files, remote_path):
 
 
 @task
+def build():
+    ''' Build the code locally. '''
+    config = get_config()
+    stage = shell.get_stage()
+
+    buildman.build(stage, config)
+
+
+@task
 def deploy():
     ''' Zero-Downtime deployment for the backend. '''
     config = get_config()
